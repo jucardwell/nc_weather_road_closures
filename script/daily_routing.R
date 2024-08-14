11fa56de93cbd6e939e04312dfe608e62f1e53ca
jucardwell <91475282+jucardwell@users.noreply.github.com>
2024-08-14 10:40:55 -0400

    Update and rename daily_routing.R to script/daily_routing.R

--- script/daily_routing.R
+++ script/daily_routing.R
@@ -2,18 +2,18 @@ library(tidyverse)
 library(dodgr)
 
 #read in osm data
-nc_roads <- dodgr_load_streetnet("data/nc_weighted_network.Rds")
+nc_roads <- dodgr_load_streetnet("../data/nc_weighted_network.Rds")
 nc_roads <- dodgr_contract_graph(nc_roads)
 #get largest component
 largest_component <- nc_roads[nc_roads$component == 1, ]
 
 #routing data
-comp_df <- read_csv("data/local_travel_baseline.csv")
-bg_o <- read_csv("data/bg_o.csv") %>% mutate(node_id = as.character(node_id))
-bg_d <- read_csv("data/bg_d.csv") %>% mutate(node_id = as.character(node_id))
+comp_df <- read_csv("../data/local_travel_baseline.csv")
+bg_o <- read_csv("../data/bg_o.csv") %>% mutate(node_id = as.character(node_id))
+bg_d <- read_csv("../data/bg_d.csv") %>% mutate(node_id = as.character(node_id))
 
 #get all closure data
-closure_files <- list.files("data/daily_road_closures/") 
+closure_files <- list.files("../data/daily_road_closures/") 
 
 #go through each daily file in the year
 for (i in 1:length(filtered_files)){
@@ -27,7 +27,7 @@ for (i in 1:length(filtered_files)){
   #if it is the first iteration, run the entire process
   if(i == 1){
     #get closures for the day
-    closures <- read_csv(paste("data/daily_road_closures/", file_name, sep = ""))
+    closures <- read_csv(paste("../data/daily_road_closures/", file_name, sep = ""))
     
     #filter road network to just open roads
     open <- largest_component[!(largest_component$edge_id %in% closures$edge_list),]
@@ -55,16 +55,16 @@ for (i in 1:length(filtered_files)){
     merged_df <- merge(comp_df, pivoted_df, by = c("o_fid", "d_fid"), all.x = TRUE) %>% mutate(diff = round(closure_secs_travel/60, 2) - round(baseline_secs_travel/60, 2))
     
     #write to .csv
-    write_csv(merged_df, paste("output/local_travel/", updated_file_name, "local_travel_closures.csv", sep = ""))
+    write_csv(merged_df, paste("../output/local_travel/", updated_file_name, "local_travel_closures.csv", sep = ""))
     
     ##if it isn't the first iteration, check if the closures for this day are the exactly same as from the day before
   } else{
     #get name of the previous day
     previous_file_name <- filtered_files[i-1]
     #get data for the current day
-    closures <- read_csv(paste("data/daily_road_closures/", file_name, sep = ""))
+    closures <- read_csv(paste("../data/daily_road_closures/", file_name, sep = ""))
     #read in data for the previos day
-    previous_file <- read_csv(paste("data/daily_road_closures/", previous_file_name, sep = ""))
+    previous_file <- read_csv(paste("../data/daily_road_closures/", previous_file_name, sep = ""))
     
     ##if there are 0 rows do nothing
     if(nrow(closures) == 0){
@@ -102,7 +102,7 @@ for (i in 1:length(filtered_files)){
       merged_df <- merge(comp_df, pivoted_df, by = c("o_fid", "d_fid"), all.x = TRUE) %>% mutate(diff = round(closure_secs_travel/60, 2) - round(baseline_secs_travel/60, 2))
       
       #write to .csv
-      write_csv(merged_df, paste("output/local_travel/", updated_file_name, "local_travel_closures.csv", sep = ""))
+      write_csv(merged_df, paste("../output/local_travel/", updated_file_name, "local_travel_closures.csv", sep = ""))
     }
   }
 }
