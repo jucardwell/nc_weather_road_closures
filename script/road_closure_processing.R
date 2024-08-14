4237ec9ac09f360910b22d3240320dfae7d06e45
jucardwell <91475282+jucardwell@users.noreply.github.com>
2024-08-14 10:44:02 -0400

    Update and rename road_closure_processing.R to script/road_closure_processing.R

--- script/road_closure_processing.R
+++ script/road_closure_processing.R
@@ -9,7 +9,7 @@ library(stringdist)
 joined_closures<- read_csv("") %>% filter(!is.na(edge_id))
 
 #closure data
-closure_data <- read_csv("data/aggregated_road_closures.csv") %>% mutate(edge_id = "", way_id = "", match = 0, oneway = 0)
+closure_data <- read_csv("../data/aggregated_road_closures.csv") %>% mutate(edge_id = "", way_id = "", match = 0, oneway = 0)
 
 #loop too add spatial join information to closure data
 for (i in 1:nrow(closure_data)){
@@ -30,5 +30,5 @@ matched_closures <- closure_data %>% filter(nchar(edge_id) > 0)
 non_matched_closures <- closure_data %>% filter(nchar(edge_id) == 0)
 
 #write new closure dataset
-write_csv(matched_closures, "data/matched_closures.csv")
-write_csv(non_matched_closures, "data/unmatched_closures.csv")
+write_csv(matched_closures, "../data/matched_closures.csv")
+write_csv(non_matched_closures, "../data/unmatched_closures.csv")
