09a204c1646c4873026ea6a0ee6d6b73ac597b23
jucardwell <91475282+jucardwell@users.noreply.github.com>
2024-08-14 10:38:35 -0400

    Update and rename baseline_local_routing.R to script/baseline_local_routing.R

--- script/baseline_local_routing.R
+++ script/baseline_local_routing.R
@@ -3,21 +3,21 @@ library(dodgr)
 library(tidyverse)
 
 #read in osm data
-nc_roads <- dodgr_load_streetnet("data/nc_weighted_network.Rds")
+nc_roads <- dodgr_load_streetnet("../data/nc_weighted_network.Rds")
 nc_roads <- dodgr_contract_graph(nc_roads)
 #get largest component
 largest_component <- nc_roads[nc_roads$component == 1, ]
 
 
 #read in local neighbor file
-neighbor <- read_csv("data/local_travel_od.csv")
-bg <- read_csv("data/bg_simp_routable_manual.csv") %>% mutate(node_id = as.character(node_id))
+neighbor <- read_csv("../data/local_travel_od.csv")
+bg <- read_csv("../data/bg_simp_routable_manual.csv") %>% mutate(node_id = as.character(node_id))
 
 bg_o <- bg %>% filter(fid %in% neighbor$o_fid)  
 bg_d <- bg %>% filter(fid %in% neighbor$d_fid)
 
-write_csv(bg_o, "data/bg_o.csv")
-write_csv(bg_o, "data/bg_d.csv")
+write_csv(bg_o, "../data/bg_o.csv")
+write_csv(bg_o, "../data/bg_d.csv")
 
 from <- bg_o$node_id
 to <- bg_d$node_id
@@ -40,5 +40,5 @@ pivoted_df <- time_dataframe %>% pivot_longer(cols = !o_fid, names_to = "d_fid",
 merged_neighbor <- merge(neighbor, pivoted_df, by = c("o_fid", "d_fid"))
 
 #write final file
-write_csv(merged_neighbor, "data/local_travel_baseline.csv")
+write_csv(merged_neighbor, "../data/local_travel_baseline.csv")
 
