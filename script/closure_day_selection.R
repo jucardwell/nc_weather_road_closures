7621984a872b17ff12bd725c947e42e050732077
jucardwell <91475282+jucardwell@users.noreply.github.com>
2024-08-14 10:39:14 -0400

    Update and rename closure_day_selection.R to script/closure_day_selection.R

--- script/closure_day_selection.R
+++ script/closure_day_selection.R
@@ -4,7 +4,7 @@
 library(tidyverse)
 
 #read in closure data
-closure_data <- read_csv("data/dodgr_id_road_closures.csv")
+closure_data <- read_csv("../data/dodgr_id_road_closures.csv")
 
 #get a list of all days from 01-01-2016 to 07-05-2023
 list_days <- seq(as.Date("2016-01-01"), as.Date("2023-07-05"), by="days")
@@ -16,5 +16,5 @@ for (i in 1:length(list_days)){
   filtered_data <- closure_data %>% filter(start_day <= date & end_day >= date) 
   edge_list <- unlist(strsplit(filtered_data$edge_id_dodgr, ", "))
   df <- as.data.frame(edge_list)
-  write_csv(df, paste("data/daily_road_closures/", date, ".csv", sep = ""))
+  write_csv(df, paste("../data/daily_road_closures/", date, ".csv", sep = ""))
 }
