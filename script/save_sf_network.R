7efc08da0e5f44013702b3a5eb52bbfec9fee94c
jucardwell <91475282+jucardwell@users.noreply.github.com>
2024-08-14 10:44:22 -0400

    Update and rename save_sf_network.R to script/save_sf_network.R

--- script/save_sf_network.R
+++ script/save_sf_network.R
@@ -5,9 +5,9 @@ library(dodgr)
 library(sf)
 
 #read in osm data
-nc_roads <- dodgr_load_streetnet("data/nc_weighted_network.Rds")
+nc_roads <- dodgr_load_streetnet("../data/nc_weighted_network.Rds")
 
 #to sf object
 sf_obj <- dodgr_to_sf(nc_roads)
 
-st_write(sf_obj, "data/nc_sf_network.shp")
+st_write(sf_obj, "../data/nc_sf_network.shp")
