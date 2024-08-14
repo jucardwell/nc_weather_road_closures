c4f71c65499da8ced405058118a9763f3e045eb0
jucardwell <91475282+jucardwell@users.noreply.github.com>
2024-08-14 10:37:35 -0400

    Update and rename aggregate_local_travel_closure.R to script/aggregate_local_travel_closure.R

--- script/aggregate_local_travel_closure.R
+++ script/aggregate_local_travel_closure.R
@@ -5,7 +5,7 @@ library(tidyverse)
 library(sf)
 
 #get list of closure files
-closure_files <- list.files("output/local_travel/", full.names = TRUE)
+closure_files <- list.files("../output/local_travel/", full.names = TRUE)
 
 #get block groups to get population data
 cbgs <- st_read("")
@@ -15,7 +15,7 @@ for (day in closure_files){
   #extract date
 date <- substr(day, 48, 57)
 #create new file name
-new_file_name <- paste("output/agg_local_travel/agg_daily_analysis_", date, ".csv", sep = "")
+new_file_name <- paste("../output/agg_local_travel/agg_daily_analysis_", date, ".csv", sep = "")
 #calculate number of trips, replace any negatives with 0s
 daily_file <- read_csv(day) %>% left_join(cbgs, join_by("o_fid" == "fid")) %>% mutate(trips = (perc/100) * POP20, diff = ifelse(diff < 0, 0, diff), total_diff = diff * trips)
 #aggregate to cbg
